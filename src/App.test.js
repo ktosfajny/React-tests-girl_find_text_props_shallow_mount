3c8964fb91e97227ef10a37771b4bdc4c56ea7f3
kamil <kamil@kamil.kamil>
2021-02-07 18:15:39 +0100

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,101 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import AccountBalance from "./components/AccountBalance";
+import Notification from "./components/Notification";
+
+import { shallow, mount } from "enzyme"; // 'shallow' renderuje komponent ale bez jego dzieci a 'mount' renderuje komponent razem z dziećmi
+import toJson from "enzyme-to-json";
+
+const userBalance = {
+  balance: 1100,
+  savingBalance: 103,
+};
+
+// describe pozwala na zgrupowanie kilku testów
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders App component header without crashing", () => {
+    const wrapper = shallow(<App />);
+    const header = (
+      <h1 className="has-text-centered title is-1">
+        Welcome in the personal finance app!
+      </h1>
+    );
+    expect(wrapper.contains(header)).toEqual(true);
+  });
+
+  it("renders Notification component without crashing", () => {
+    shallow(<Notification />);
+  });
+
+  it("renders button", () => {
+    const wrapper = mount(<AccountBalance accounts={userBalance} />);
+    const label = wrapper.find("#balance-button").text(); // find pozwala na znalezienie elementu czyli taki jakby querySelector, .text() wybiera wartość textową
+    expect(label).toEqual("Send 100$");
+  });
+});
+
+describe("passing props", () => {
+  const accountWrapper = mount(<AccountBalance accounts={userBalance} />);
+  const notificationWrapper = mount(
+    <Notification balance={userBalance.balance} />
+  );
+
+  it("accepts user account props", () => {
+    expect(accountWrapper.props().accounts).toEqual(userBalance); // trzeba wywołać funkcję .props() żeby dostać się do propów
+  });
+
+  it("contains saving Balance value", () => {
+    const value = accountWrapper.find(".savings").text();
+    const expectedValue = userBalance.savingBalance + "$";
+    expect(value).toEqual(expectedValue);
+  });
+
+  it("notification accepts props", () => {
+    expect(notificationWrapper.props().balance).toEqual(userBalance.balance);
+  });
+});
+
+describe("logic", () => {
+  const wrapper = mount(<AccountBalance accounts={userBalance} />);
+  const notificationWrapper = mount(
+    <Notification balance={userBalance.balance} />
+  );
+  wrapper.find("#balance-button").simulate("click");
+  it("button click - update savings", () => {
+    const savingsValue = wrapper.find(".savings").text();
+    const expectedValue = userBalance.savingBalance + 100 + "$";
+    expect(savingsValue).toEqual(expectedValue);
+  });
+
+  it("button click - update balance", () => {
+    const balanceValue = wrapper.find(".balance").text();
+    const expectedBalanceValue = userBalance.balance - 100 + "$";
+    expect(balanceValue).toEqual(expectedBalanceValue);
+  });
+});
+
+describe("snapshots", () => {
+  it("App snapschots", () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it("Accounts snapshot", () => {
+    const accountBalanceTree = shallow(
+      <AccountBalance accounts={userBalance} />
+    );
+    expect(toJson(accountBalanceTree)).toMatchSnapshot();
+  });
+
+  it("Notification snapschot", () => {
+    const notificationTree = shallow(
+      <Notification balance={userBalance.balance} />
+    );
+    expect(toJson(notificationTree)).toMatchSnapshot();
+  });
 });
